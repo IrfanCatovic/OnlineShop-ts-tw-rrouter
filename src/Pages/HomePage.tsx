bf888455e8a59628261f6078a1a445d9d2b03dc7
IrfanCatovic <catovicc84@gmail.com>
2026-01-20 15:51:15 +0100

    Problem with adding border in tailwind

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import CreateUser from "../features/User/createUser"
 
 export default function HomePage(){
-    return <div>
-        <CreateUser />
+    return <div className="w-full">
+        <CreateUser/>
     </div>
 }
