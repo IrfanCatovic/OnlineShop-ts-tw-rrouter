005bb70a35201eb0e0ac49c9f7f1c4c4b6c300eb
IrfanCatovic <catovicc84@gmail.com>
2026-01-25 16:50:51 +0100

    init gorilaMux, make routes with SetuRouter, upalili server na 8080

---
+++ backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"backend/router"
+)
+
+func main() {
+	r := router.SetupRouter() //pravi sve rute
+
+	log.Println("Server running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", r)) //pali server - port :8080
+}
